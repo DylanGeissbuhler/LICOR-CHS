0eed8ff6b15c2c9ae16cb62967569639533e4553
DylanGeissbuhler <94467032+DylanGeissbuhler@users.noreply.github.com>
2022-03-25 15:48:18 +0100

    Update LICOR_curve_transform.R

--- LICOR_curve_transform.R
+++ LICOR_curve_transform.R
@@ -3,7 +3,8 @@ dev.off()
 options(scipen = 999)
 
 #load libraries & install packages if necessary-------------------------------------
-load.lib = c("tidyverse", "DescTools", "plotly", "data.table", "purrr", "stringr", "pastecs","readxl","MASS", "ggplot2", "pracma", "signal","gridGraphics", "pdftools", "outliers", "lubridate")   
+load.lib = c("tidyverse", "DescTools", "plotly", "data.table", "purrr", "stringr", "pastecs","readxl","MASS",
+             "ggplot2", "pracma", "signal","gridGraphics", "pdftools", "outliers", "lubridate", "quantmod")   
 install.lib = load.lib[!load.lib %in% installed.packages()]
 for(lib in install.lib) install.packages(lib,dependencies=TRUE)
 sapply(load.lib,require,character=TRUE)
@@ -11,24 +12,55 @@ sapply(load.lib,require,character=TRUE)
 fileName = file.choose() # when prompted, choose the appropriate file 
 
 #Dataframe creation
-data <- read.delim(fileName, header = T, skip = 1, col.names = c("Date","Time","CO2","H2O","TH2O","TCell","PCell","Abs1","Abs2","Volt","NA"))
+data <- read.delim(fileName, header = T, skip = 1, col.names = c("Date","Time","CO2","H2O","TH2O","TCell","PCell","Abs1","Abs2","Volt","ind"))
+data[11] <- 1:nrow(data)
 
-#Conversion of elapsed time into seconds 
+# Conversion of elapsed time into seconds 
 seconds <-  period_to_seconds(hms(data$Time))
 data$SecTime = seconds[]-seconds[1]
 
+# Baseline correction
+baseline <- 365
+data$CO2 <- data$CO2 - baseline
+
 # Graph to visualize the concentration peak
-(CO2 <- plot_ly(data = data, x = ~SecTime, y = ~CO2, name = 'CO2', mode = 'lines', type = 'scatter') %>%
-  layout(title = "CO2 Curve", xaxis = list(title="Time"), yaxis = list(title="CO2 [ppm]")))
+(CO2 <- plot_ly(data = data, x = ~ind, y = ~CO2, name = 'CO2', mode = 'lines', type = 'scatter') %>%
+  layout(xaxis = list(title="Time (s)"), yaxis = list(title="CO2 [ppm]")))
+
 
 # Calculation of the flux of C curve
-flow <- 1  # in ml/sec
-Vcell <- 14.5 # in ml
+flow <- 55 # in ml/min
+flowliter <- flow/(1000*60)  # in L/sec
 
-data$Cflux <- ((12.01/Vcell)*(data$PCell*flow)/(8.3145*(data$TCell+ 273.15)))*data$CO2 # Gives a curve of C in microgramms per sec
+
+# Curve of C in microgramms per sec (ideal gas law), from the ppm and flow
+data$Cflux <- ((12.01)*(data$PCell*flow)/(8.3145*(data$TCell+ 273.15)))*data$CO2
 
 (Cflux <- plot_ly(data = data, x = ~SecTime, y = ~Cflux, name = 'C', mode = 'lines', type = 'scatter') %>%
   layout(title = "C Flux", xaxis = list(title="Time (s)"), yaxis = list(title="C [micrograms/s]")))
 
-#Calculation of the total amount of emitted C
-TotalC <-  AUC(data$SecTime, data$Cflux, method = "trapezoid")
+# Manual method to detect peaks
+# CO2 peaks
+valuePeak <- 1800
+
+max <- which(diff(sign(diff(data$CO2, lag = 2)))==-2)+1 # Indices at which local max or min are detected
+peaksInd <- which(data$CO2[max]>valuePeak) # Indices of max which correspond to actual peaks 
+maxInd <- max[peaksInd] # Indices of these in the actual data
+
+peaksVal <- data$CO2[maxInd] # Value of the peaks [ppm]
+peaksValFlux <- data$Cflux[maxInd] # Value of the peaks [micrograms]
+
+# Integration of the CO2 peaks
+
+CarbonCO2 <-  data.frame(matrix(NA, nrow = length(maxInd)))
+for (i in 1:length(maxInd))
+  CarbonCO2[i,1] =  AUC(data$ind[(maxInd[i]-45):(maxInd[i]+ 125)], data$CO2[(maxInd[i]-45):(maxInd[i]+ 125)], method = "trapezoid")
+
+
+# Integration  of C flux peaks
+
+CarbonF <-  data.frame(matrix(NA, nrow = length(maxInd)))
+for (i in 1:length(maxInd))
+  CarbonF[i,1] =  AUC(data$SecTime[(maxInd[i]-40):(maxInd[i]+ 120)], data$Cflux[(maxInd[i]-40):(maxInd[i]+ 120)], method = "trapezoid")/2
+
+
