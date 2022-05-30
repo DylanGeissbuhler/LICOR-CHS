3a09d81dcf4b67a96d82b25dc6b65ae13039ca21
DylanGeissbuhler <94467032+DylanGeissbuhler@users.noreply.github.com>
2022-05-30 16:25:15 +0200

    Create app.R

---
+++ app.R
@@ -0,0 +1,62 @@
+load.lib = c("tidyverse", "DescTools", "plotly", "data.table", "purrr","readxl","MASS", "pracma", "lubridate","shiny","vtable")   
+install.lib = load.lib[!load.lib %in% installed.packages()]
+for(lib in install.lib) install.packages(lib,dependencies=TRUE)
+sapply(load.lib,require,character=TRUE)
+
+options(shiny.maxRequestSize=30*1024^2)
+
+ui <- fluidPage(
+  
+  # App title
+  titlePanel("LICOR-CHS Sample CO2 Quantification"),
+
+    sidebarPanel(
+      numericInput("baseline","Enter a baseline value [ppm] :", value = 400),
+      numericInput("flow","Flowrate of CHS [mL/min] :", value = 55),
+      numericInput("threshold","Value of threshold for peak [ppm] :", value = 2000),
+      numericInput("delay","Delay between start of peak and top [s] :", value = 30),
+      numericInput("tail", "Duration of tailing [s] :", value = 120),
+      tableOutput("carbon")
+    ),
+  
+ # UI
+ mainPanel(
+   
+   fileInput("file1","Choose a .txt file (max. size 30MB) :", multiple = FALSE),
+  
+   plotlyOutput("CO2Plot", height = "1000px"),
+
+   )
+  )
+
+# Define server logic 
+server <- function(input, output, session) {
+
+  source("LICOR_integration.R", encoding = "UTF-8", echo = TRUE)
+  
+  data <- reactive(
+    pars(input$file1$datapath)
+  )
+  
+  output$contents <- renderTable({
+    req(input$file1)
+    data()
+  })
+  
+  output$CO2Plot <- renderPlotly({
+    req(input$file1)
+    
+    subplot(list(plot1(data(), input$baseline, input$threshold), 
+                 plot2(data(), input$flow, input$baseline)), 
+            nrows = 2, margin = 0.01, titleX = TRUE, titleY = TRUE,
+            shareX = TRUE)
+    
+  })
+  
+  output$carbon <- renderTable({
+    req(input$file1)
+    integ(data(), input$delay, input$tail, input$threshold, input$flow, input$baseline)
+  })
+    }
+
+shinyApp(ui = ui, server = server)
